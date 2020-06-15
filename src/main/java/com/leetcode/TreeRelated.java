a6699ed32d06af60c0e9a0bfae42a41349afa2c0
hongbing.li <hongbing.li@wbkit.com>
2020-06-15 09:19:48 +0800

    叶子相似结点

--- src/main/java/com/leetcode/TreeRelated.java
+++ src/main/java/com/leetcode/TreeRelated.java
@@ -67,9 +67,33 @@ public class TreeRelated {
     //如果有两颗二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
     //
     //如果给定的两个头结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+    //方法：深度优先搜索 dfs (深度优先搜索包含三种前序/中序/后序 ，要理解)
+    //思路和算法
+    //首先，让我们找出给定的两个树的叶值序列。之后，我们可以比较它们，看看它们是否相等。
+    //
+    //要找出树的叶值序列，我们可以使用深度优先搜索。如果结点是叶子，那么 dfs 函数会写入结点的值，然后递归地探索每个子结点。
+    // 这可以保证按从左到右的顺序访问每片叶子，因为在右孩子结点之前完全探索了左孩子结点。
+    //复杂度分析
+    //
+    //时间复杂度：O(T1 + T2)，其中 T1, T2T是给定的树的长度。
+    //空间复杂度：O(T1 + T2)，存储叶值所使用的空间。
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        return true;
+        List<Integer> leaves1 = new ArrayList();
+        List<Integer> leaves2 = new ArrayList();
+        dfs(root1, leaves1);
+        dfs(root2, leaves2);
+        return leaves1.equals(leaves2);
     }
+
+    public void dfs(TreeNode node, List<Integer> leafValues) {
+        if (node != null) {
+            if (node.left == null && node.right == null)
+                leafValues.add(node.val);
+            dfs(node.left, leafValues);
+            dfs(node.right, leafValues);
+        }
+    }
+
 
     //104. 二叉树的最大深度===>就是高度
     //给定一个二叉树，找出其最大深度。
