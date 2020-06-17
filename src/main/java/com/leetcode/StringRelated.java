5765532503661b9c4fedb13a756b386bae84a88c
hongbing.li <285396440@qq.com>
2020-06-17 15:21:17 +0800

    回溯

--- src/main/java/com/leetcode/StringRelated.java
+++ src/main/java/com/leetcode/StringRelated.java
@@ -421,4 +421,29 @@ public class StringRelated {
         return new String(sArray);
 
     }
+
+    //58. 最后一个单词的长度
+    //给定一个仅包含大小写字母和空格 ' ' 的字符串 s，返回其最后一个单词的长度。如果字符串从左向右滚动显示，那么最后一个单词就是最后出现的单词。
+    //
+    //如果不存在最后一个单词，请返回 0 。
+    //
+    //说明：一个单词是指仅由字母组成、不包含任何空格字符的 最大子字符串。
+    //
+    // 
+    //
+    //示例:
+    //
+    //输入: "Hello World"
+    //输出: 5
+    public int lengthOfLastWord(String s) {
+        String s1 = s.trim();
+        if (s1.length() == 0) return 0;
+        int start = -1;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) == ' ') {
+                start = i;
+            }
+        }
+        return s1.length() - 1 - start;
+    }
 }
