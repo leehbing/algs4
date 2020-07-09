5c2e071ff85349566b69c18fb1aa99352b684746
hongbing.li <285396440@qq.com>
2020-07-09 18:00:35 +0800

    动态规划

--- src/main/java/com/leetcode/StringRelated.java
+++ src/main/java/com/leetcode/StringRelated.java
@@ -610,4 +610,54 @@ public class StringRelated {
         return new String(chars);
     }
 
+
+    //139. 单词拆分
+    //给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+    //
+    //说明：
+    //拆分时可以重复使用字典中的单词。
+    //你可以假设字典中没有重复的单词。
+    //
+    //示例 1：
+    //输入: s = "leetcode", wordDict = ["leet", "code"]
+    //输出: true
+    //解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+    //示例 2：
+    //输入: s = "applepenapple", wordDict = ["apple", "pen"]
+    //输出: true
+    //解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+    //     注意你可以重复使用字典中的单词。
+    //示例 3：
+    //输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+    //输出: false
+    //
+    //方法一：动态规划
+    //       ""    l     e     e    t     c     o     d    e
+    //      true false false false true false false false true
+    //
+    //1.初始化 dp=[False,⋯,False]，长度为 n+1。n 为字符串长度。dp[i] 表示 s 的前 i 位是否可以用wordDict 中的单词表示。
+    //2.初始化 dp[0]=True，空字符可以被表示。
+    //3.遍历字符串的所有子串，遍历开始索引 i，遍历区间 [0,n)：
+    //  遍历结束索引 j，遍历区间 [i+1,n+1)：
+    //      若 dp[i]=True 且 s[i,⋯,j) 在 wordlistwordlist 中：dp[j]=True。解释：dp[i]=True 说明 s 的前 i 位可以用 wordDict 表示，
+    //      则 s[i,⋯,j) 出现在 wordDict 中，说明 s 的前 j 位可以表示。
+    //4.返回 dp[n]
+    //相当于动态方程： dp[j]=dp[i] && check(s[i...j])
+    //时间复杂度：O(n^2)
+    //空间复杂度：O(n)
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length() + 1; j++) {
+                if (dp[i] && wordDictSet.contains(s.substring(i , j))) {
+                    dp[j] = true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+
 }
